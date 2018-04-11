1247f302a9fcb8de8f60a7404e4ad8580240fc07
tanaka <step.by.step524@gmail.com>
2018-04-12 00:39:49 +0900

    change template engine & refactor ECT -> ejs

--- bin/index.js
+++ bin/index.js
@@ -3,6 +3,8 @@
 
 var args = process.argv.slice(2);
 
+let fs = require("fs");
+
 const help = () => {
 
 };
@@ -11,75 +13,88 @@ const generate_config = () => {
 
 };
 
-const render_html = (data) => {
-    const template_path = require('path').resolve(__dirname + '/../resource/template.ect');
+const render_html = data => {
+    let assign = {
+        "data" : data
+    };
 
-    let academic_histories = data.academic_histories || [];
-    let work_histories = data.work_histories || [];
-
-    let academic_flg = academic_histories.length == 0;
-    let work_flg = work_histories.length == 0;
-
-    data.pop_history = function (index) {
-        if (! academic_flg) {
-            academic_flg = true;
-            return {
-                "year": "",
-                "month": "",
-                "value": "学歴"
-            };
-        }
-
-        if (academic_histories.length > 0) {
-            return academic_histories.shift();
-        }
-
-        if (! work_flg) {
-            if (index == 12) { // １ページ目の最下段だったときはスキップ
+    {
+        // 表示用の関数をオブジェクトに拡張しておく
+        let academic_histories = data.academic_histories || [];
+        let work_histories = data.work_histories || [];
+    
+        let academic_flg = academic_histories.length == 0;
+        let work_flg = work_histories.length == 0;
+    
+        assign.fetch_history = (index) => {
+            if (! academic_flg) {
+                academic_flg = true;
                 return {
                     "year": "",
                     "month": "",
-                    "value": ""
+                    "value": "学歴"
                 };
             }
-            work_flg = true;
+    
+            if (academic_histories.length > 0) {
+                return academic_histories.shift();
+            }
+    
+            if (! work_flg) {
+                if (index == 12) { // １ページ目の最下段だったときはスキップ
+                    return {
+                        "year": "",
+                        "month": "",
+                        "value": ""
+                    };
+                }
+                work_flg = true;
+                return {
+                    "year": "",
+                    "month": "",
+                    "value": "職歴"
+                };
+            }
+    
+            if (work_histories.length > 0) {
+                return work_histories.shift();
+            }
+    
             return {
                 "year": "",
                 "month": "",
-                "value": "職歴"
+                "value": ""
             };
-        }
 
-        if (work_histories.length > 0) {
-            return work_histories.shift();
-        }
-
-        return {
-            "year": "",
-            "month": "",
-            "value": ""
         };
-    };
-
-    let licenses = data.licenses || [];
-    data.pop_license = function () {
-        if (licenses.length > 0) {
-            return licenses.shift();
-        }
-
-        return {
-            "year": "",
-            "month": "",
-            "value": ""
+    
+        let licenses = data.licenses || [];
+        assign.fetch_license = () => {
+            if (licenses.length > 0) {
+                return licenses.shift();
+            }
+    
+            return {
+                "year": "",
+                "month": "",
+                "value": ""
+            };
         };
-    };
 
-    return require("ect")().render(template_path, data);
+        assign.echo = (key, default_value) => {
+            if (typeof data[key] == "undefined") {
+                return default_value || "";
+            }
+            return data[key];
+        };
+    }
+
+    const template_path = require('path').resolve(__dirname + '/../resource/template.ejs');
+    let template = fs.readFileSync(template_path, 'utf8');
+    return require("ejs").render(template, assign);
 };
 
 const make_pdf = (config_file, export_file) => {
-    let fs = require("fs");
-
     let data = {};
     if (fs.existsSync(config_file)) {
         try {
@@ -92,18 +107,20 @@ const make_pdf = (config_file, export_file) => {
 
     let html = render_html(data);
 
-    // fs.writeFileSync("./hoge.html", html);
+    fs.writeFileSync("./hoge.html", html);
 
-    let options = { format: "A4" };
-
-    require("html-pdf").create(html, options).toFile(export_file, function(err, res) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log(res); // { filename: '/app/businesscard.pdf' }
-    });
     
-    process.cwd();
+
+    // let options = { format: "A4" };
+
+    // require("html-pdf").create(html, options).toFile(export_file, function(err, res) {
+    //     if (err) {
+    //         return console.log(err);
+    //     }
+    //     console.log(res); // { filename: '/app/businesscard.pdf' }
+    // });
+    
+    // process.cwd();
 };
 
 make_pdf("resume.json", "resume.pdf");
