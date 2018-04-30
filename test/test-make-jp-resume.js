7fb77b566626c1a105f5286da117d548df902430
tanaka <step.by.step524@gmail.com>
2018-04-30 23:15:14 +0900

    add sample unittest

---
+++ test/test-make-jp-resume.js
@@ -0,0 +1,13 @@
+#!/usr/bin/env node
+"use strict";
+
+let assert = require('assert');
+let resume = require('../');
+
+describe("test render html", () => {
+    it('hoge', () => {
+        assert.equal(1, 1);
+    });
+    
+    
+});
